08f79ffb3f2fe93ed03824be65555fcdcc2ee22c
Noisyfox <timemanager.rick@gmail.com>
2012-08-23 21:17:24 +0800

    简化代码

--- src/org/foxteam/noisyfox/THEngine/Performers/PowerUp.java
+++ src/org/foxteam/noisyfox/THEngine/Performers/PowerUp.java
@@ -16,7 +16,10 @@
  */
 package org.foxteam.noisyfox.THEngine.Performers;
 
+import org.foxteam.noisyfox.FoxGaming.Core.MathsHelper;
 import org.foxteam.noisyfox.FoxGaming.Core.Stage;
+import org.foxteam.noisyfox.FoxGaming.G2D.GraphicCollision;
+import org.foxteam.noisyfox.FoxGaming.G2D.Sprite;
 
 /**
  * @ClassName: PowerUp
@@ -26,6 +29,66 @@ import org.foxteam.noisyfox.FoxGaming.Core.Stage;
  * 
  */
 public abstract class PowerUp extends Bullet {
+
+	@Override
+	protected void onCreate() {
+		this.setAlarm(0, (int) (Stage.getSpeed() * 3.0f), true);
+		this.startAlarm(0);
+
+		this.motion_set(MathsHelper.random(0, 359), 50f / Stage.getSpeed());
+	}
+
+	@Override
+	protected void onAlarm(int whichAlarm) {
+		if (whichAlarm == 0) {
+			this.motion_set(MathsHelper.random(0, 359), 50f / Stage.getSpeed());
+		}
+	}
+
+	@Override
+	protected void onStep() {
+		// 判断当前位置
+		int left = getSprite().getOffsetX() + 20;
+		int top = getSprite().getOffsetY() + 20;
+		int right = Stage.getCurrentStage().getWidth()
+				+ (getSprite() == null ? 0
+						: (getSprite().getOffsetX() - getSprite().getWidth()))
+				- 20;
+		int bottom = Stage.getCurrentStage().getHeight()
+				+ (getSprite() == null ? 0
+						: (getSprite().getOffsetY() - getSprite().getHeight()))
+				- 20;
+
+		if (getX() < left) {
+			if (getY() < top && direction <= 270) {
+				this.motion_set(MathsHelper.random(271, 359),
+						50f / Stage.getSpeed());
+			} else if (getY() > bottom && direction >= 90) {
+				this.motion_set(MathsHelper.random(1, 89),
+						50f / Stage.getSpeed());
+			} else if (direction >= 90 && direction <= 270) {
+				this.motion_set(
+						MathsHelper.degreeIn360(MathsHelper.random(-89, 89)),
+						50f / Stage.getSpeed());
+			}
+		} else if (getX() > right) {
+			if (getY() < top && (direction <= 180 || direction >= 270)) {
+				this.motion_set(MathsHelper.random(181, 269),
+						50f / Stage.getSpeed());
+			} else if (getY() > bottom && (direction <= 90 || direction >= 180)) {
+				this.motion_set(MathsHelper.random(91, 179),
+						50f / Stage.getSpeed());
+			} else if (direction <= 90 || direction >= 270) {
+				this.motion_set(MathsHelper.random(91, 269),
+						50f / Stage.getSpeed());
+			}
+		} else if (getY() < top && direction <= 180) {
+			this.motion_set(MathsHelper.random(181, 359),
+					50f / Stage.getSpeed());
+		} else if (getY() > bottom && direction >= 180) {
+			this.motion_set(MathsHelper.random(1, 179), 50f / Stage.getSpeed());
+		}
+	}
 
 	public PowerUp(int x, int y) {
 		this.perform(Stage.getCurrentStage().getStageIndex());
--- src/org/foxteam/noisyfox/THEngine/Performers/PowerUps/PowerUp_Missile_Guided.java
+++ src/org/foxteam/noisyfox/THEngine/Performers/PowerUps/PowerUp_Missile_Guided.java
@@ -16,8 +16,6 @@
  */
 package org.foxteam.noisyfox.THEngine.Performers.PowerUps;
 
-import org.foxteam.noisyfox.FoxGaming.Core.MathsHelper;
-import org.foxteam.noisyfox.FoxGaming.Core.Stage;
 import org.foxteam.noisyfox.FoxGaming.G2D.GraphicCollision;
 import org.foxteam.noisyfox.FoxGaming.G2D.Sprite;
 import org.foxteam.noisyfox.THEngine.Performers.Hitable;
@@ -36,10 +34,6 @@ public class PowerUp_Missile_Guided extends PowerUp {
 
 	public PowerUp_Missile_Guided(int x, int y) {
 		super(x, y);
-	}
-
-	@Override
-	protected void onCreate() {
 		Sprite bulletSprite = new Sprite();
 		bulletSprite
 				.loadFromBitmap(
@@ -54,59 +48,6 @@ public class PowerUp_Missile_Guided extends PowerUp {
 				-bulletSprite.getHeight() / 2, bulletSprite.getWidth(),
 				bulletSprite.getHeight());
 		this.bindCollisionMask(co);
-
-		this.setAlarm(0, (int) (Stage.getSpeed() * 3.0f), true);
-		this.startAlarm(0);
-
-		this.motion_set(MathsHelper.random(0, 359), 50f / Stage.getSpeed());
-	}
-
-	@Override
-	protected void onAlarm(int whichAlarm) {
-		if (whichAlarm == 0) {
-			this.motion_set(MathsHelper.random(0, 359), 50f / Stage.getSpeed());
-		}
-	}
-
-	@Override
-	protected void onStep() {
-		// 判断当前位置
-		int left = getSprite().getOffsetX() + 20;
-		int top = getSprite().getOffsetY() + 20;
-		int right = Stage.getCurrentStage().getWidth()
-				+ getSprite().getOffsetX() - getSprite().getWidth() - 20;
-		int bottom = Stage.getCurrentStage().getHeight()
-				+ getSprite().getOffsetY() - getSprite().getHeight() - 20;
-
-		if (getX() < left) {
-			if (getY() < top && direction <= 270) {
-				this.motion_set(MathsHelper.random(271, 359),
-						50f / Stage.getSpeed());
-			} else if (getY() > bottom && direction >= 90) {
-				this.motion_set(MathsHelper.random(1, 89),
-						50f / Stage.getSpeed());
-			} else if (direction >= 90 && direction <= 270) {
-				this.motion_set(
-						MathsHelper.degreeIn360(MathsHelper.random(-89, 89)),
-						50f / Stage.getSpeed());
-			}
-		} else if (getX() > right) {
-			if (getY() < top && (direction <= 180 || direction >= 270)) {
-				this.motion_set(MathsHelper.random(181, 269),
-						50f / Stage.getSpeed());
-			} else if (getY() > bottom && (direction <= 90 || direction >= 180)) {
-				this.motion_set(MathsHelper.random(91, 179),
-						50f / Stage.getSpeed());
-			} else if (direction <= 90 || direction >= 270) {
-				this.motion_set(MathsHelper.random(91, 269),
-						50f / Stage.getSpeed());
-			}
-		} else if (getY() < top && direction <= 180) {
-			this.motion_set(MathsHelper.random(181, 359),
-					50f / Stage.getSpeed());
-		} else if (getY() > bottom && direction >= 180) {
-			this.motion_set(MathsHelper.random(1, 179), 50f / Stage.getSpeed());
-		}
 	}
 
 	@Override
