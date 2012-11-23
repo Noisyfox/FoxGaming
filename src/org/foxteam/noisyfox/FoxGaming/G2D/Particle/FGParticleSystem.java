5f58942e297c49fa4789d99072f523b901733cbd
Noisyfox <timemanager.rick@gmail.com>
2012-11-23 13:00:18 +0800

    增加部分方法声明

--- src/org/foxteam/noisyfox/FoxGaming/G2D/Particle/FGParticleSystem.java
+++ src/org/foxteam/noisyfox/FoxGaming/G2D/Particle/FGParticleSystem.java
@@ -1,5 +1,42 @@
 package org.foxteam.noisyfox.FoxGaming.G2D.Particle;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+
 public class FGParticleSystem {
+
+	public void update() {
+
+	}
+
+	public void draw(Canvas c, int x, int y) {
+
+	}
+
+	public void updateAndDraw(Canvas c, int x, int y) {
+		update();
+		draw(c, x, y);
+	}
+
+	public void setDrawOrder(boolean old2new) {
+
+	}
+
+	public void createParticle(FGParticleType type, int x, int y, int number) {
+
+	}
+
+	public void createParticle(FGParticleType type, int x, int y, Color color,
+			int number) {
+
+	}
+
+	public void clear() {
+
+	}
+	
+	public int count() {
+		return 0;
+	}
 
 }
--- src/org/foxteam/noisyfox/FoxGaming/G2D/Particle/FGParticleType.java
+++ src/org/foxteam/noisyfox/FoxGaming/G2D/Particle/FGParticleType.java
@@ -1,5 +1,81 @@
 package org.foxteam.noisyfox.FoxGaming.G2D.Particle;
 
+import org.foxteam.noisyfox.FoxGaming.G2D.FGSprite;
+
+import android.graphics.Color;
+
 public class FGParticleType {
 
+	FGSprite particleSprite = null;
+
+	public void setSprite(FGSprite sprite) {
+
+	}
+
+	public void setSpriteFrameAnimation(boolean enableFrameAni, double speed,
+			boolean startWithRandomFrame) {
+
+	}
+
+	public void setSize(double minSize, double maxSize,
+			double incrementPerStep, double wiggle) {
+
+	}
+
+	public void setScale(float xScale, float yScale) {
+
+	}
+
+	public void setOrientation(float minAngle, float maxAngle,
+			double incrementPerStep, double wiggle, boolean relative) {
+
+	}
+
+	public void setColor(Color color) {
+
+	}
+
+	public void setColor(Color color1, Color color2) {
+
+	}
+
+	public void setColor(Color color1, Color color2, Color color3) {
+
+	}
+
+	public void setColorRGB(int minR, int minG, int minB, int maxR, int maxG,
+			int maxB) {
+
+	}
+
+	public void setColorHSV(int minH, int minS, int minV, int maxH, int maxS,
+			int maxV) {
+
+	}
+
+	public void setLifeTime(int min, int max) {
+
+	}
+
+	public void createNewParticleOnStep(boolean enabled, FGParticleType type,
+			int number) {
+	}
+
+	public void createNewParticleOnDeath(boolean enabled, FGParticleType type,
+			int number) {
+	}
+
+	public void setSpeed(double minSpeed, double maxSpeed,
+			double incrementPerStep, double wiggle) {
+
+	}
+
+	public void setDirection(double minDirection, double maxDirection,
+			double incrementPerStep, double wiggle) {
+
+	}
+
+	public void setGravity(double amount, double direction) {
+
+	}
 }
