1cc3fd348c20a8d6efefc695f9d4f7849635472d
Noisyfox <timemanager.rick@gmail.com>
2013-01-18 02:53:34 +0800

    增加了方法注册，提高执行效率

--- jni/foxgaming.cpp
+++ jni/foxgaming.cpp
@@ -1,15 +1,47 @@
+#include <stdlib.h>
+
 #include <android/log.h>
 
 #include <FoxGaming.h>
 #include <MathsHelper.h>
+#include <MethodDefinations.h>
 
-#define  LOG_TAG    "libfoxgaming_particle_system"
+#define  LOG_TAG    "libfoxgaming"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
-bool initalizeJNI() {
+bool registerMethodsClazzName(JNIEnv * env, JNINativeMethod * gMethods,
+		const char* className) {
+	jclass clazz;
+	clazz = env->FindClass(className);
+	if (clazz == NULL) {
+		return JNI_FALSE;
+	}
+	if (env->RegisterNatives(clazz, gMethods,
+			sizeof(gMethods) / sizeof(gMethods[0])) < 0) {
+		return JNI_FALSE;
+	}
+
+	return JNI_TRUE;
+}
+
+bool registerMethods(JNIEnv * env) {
+	if (!registerMethodsClazzName(env, gMethods_ParticleSystem,
+			JNIREG_CLASS_ParticleSystem)) {
+		return JNI_FALSE;
+	}
+	return JNI_TRUE;
+}
+
+bool initalizeJNI(JNIEnv * env) {
 	//开始初始化
 	LOGI("JNI loaded! Start initalizing!");
+	//注册函数
+	if (registerMethods(env)) {
+		LOGI("Register methods success!");
+	} else {
+		LOGI("Register methods failed!");
+	}
 	//初始化数学函数
 	randomGauss();
 
@@ -20,5 +52,5 @@ bool initalizeJNI() {
 //--------------------------------------------------------------------------------------------
 JNIEXPORT jboolean JNICALL Java_org_foxteam_noisyfox_FoxGaming_Core_FGNativeHelper_initalizeJNINative(
 		JNIEnv * env, jclass clazz) {
-	return initalizeJNI() ? JNI_TRUE : JNI_FALSE;
+	return initalizeJNI(env) ? JNI_TRUE : JNI_FALSE;
 }
